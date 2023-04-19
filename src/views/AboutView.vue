13c08850243e2501df79928e09d3b6c7c2f4805d
Ivan Mrsulja <mrsuljaivancic@gmail.com>
2023-04-19 13:55:32 +0200

    Configured pinia, added vuetify 3.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h1>Count: {{ counter.count }}</h1>
+    <h1>Double count: {{ counter.doubleCount }}</h1>
+    <v-btn @click="counter.increment()">Increment counter</v-btn>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<script>
+import { defineComponent } from 'vue';
+import { useCounterStore } from '../stores/counter';
+
+export default defineComponent({
+  setup() {
+    const counter = useCounterStore();
+
+    return {
+      counter
+    };
+  },
+});
+</script>
