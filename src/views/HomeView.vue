0a6d20b5c1c123ff9d885b73928a9ba682161492
Ivan Mrsulja <mrsuljaivancic@gmail.com>
2023-12-28 11:01:27 +0100

    Cleaned up project, configured router.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,14 @@
 <template>
-    <hello-world />
+    <h1>HOME</h1>
 </template>
 
 <script lang="ts">
     import { defineComponent } from "vue";
 
-    // Components
-    import HelloWorld from "../components/HelloWorld.vue";
-
     export default defineComponent({
         name: "HomeView",
 
         components: {
-            HelloWorld,
         },
     });
 </script>
