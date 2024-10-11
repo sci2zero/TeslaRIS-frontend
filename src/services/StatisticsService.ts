77ab1d0f18e49e3ae342501be680948efe0cd8bf
Ivan Mrsulja <mrsuljaivancic@gmail.com>
2024-10-11 11:53:04 +0200

    Small bugfix.

--- src/services/StatisticsService.ts
+++ src/services/StatisticsService.ts
@@ -4,16 +4,18 @@ import axios from "axios";
 
 export class StatisticsService extends BaseService {
 
+    private static idempotencyKey: string = super.generateIdempotencyKey();
+
   async registerPersonView(personId: number): Promise<AxiosResponse<void>> {
-    return super.sendRequest(axios.post, `statistics/person/${personId}`);
+    return super.sendRequest(axios.post, `statistics/person/${personId}`, {}, StatisticsService.idempotencyKey);
   }
 
   async registerOUView(organisationUnitId: number): Promise<AxiosResponse<void>> {
-    return super.sendRequest(axios.post, `statistics/organisation-unit/${organisationUnitId}`);
+    return super.sendRequest(axios.post, `statistics/organisation-unit/${organisationUnitId}`, {}, StatisticsService.idempotencyKey);
   }
 
   async registerDocumentView(documentId: number): Promise<AxiosResponse<void>> {
-    return super.sendRequest(axios.post, `statistics/document/${documentId}`);
+    return super.sendRequest(axios.post, `statistics/document/${documentId}`, {}, StatisticsService.idempotencyKey);
   }
 }
 
