1b08162e663684fa8e9d0787499e2b37ee3604ef
Milos Popovic <misa01523@gmail.com>
2025-04-28 12:56:31 +0200

    Updated versions info

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { version } from './package.json'
 
 // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
 import vuetify from "vite-plugin-vuetify";
@@ -13,5 +14,8 @@ export default defineConfig({
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
+    },
+    define: {
+        'import.meta.env.VITE_APP_VERSION_PACKAGE': JSON.stringify(version)
     },
 });
