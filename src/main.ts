a7ffab83703a715967f741e76a00099aa6da5031
Milos Popovic <misa01523@gmail.com>
2025-08-14 12:43:56 +0200

    Integrate Tailwind CSS into the project, adjusted css

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import router from "./router";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
 import { createPinia } from "pinia";
+
+import './assets/base.css';
 import './assets/main.css';
 import i18n from './i18n';
 import { createHead } from "@vueuse/head";
@@ -15,9 +17,9 @@ loadFonts();
 i18n.setup();
 
 createApp(App)
-.use(router)
-.use(pinia)
-.use(vuetify)
-.use(i18n.vueI18n)
-.use(head)
-.mount("#app");
+    .use(router)
+    .use(pinia)
+    .use(vuetify)
+    .use(i18n.vueI18n)
+    .use(head)
+    .mount("#app");
