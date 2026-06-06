7ce790cb25a3224a04cd8aa8f935142f46655afc
Rade Pejanović <radepejanovic@gmail.com>
2026-06-06 23:36:45 +0200

    Added initial Funding related models

---
+++ src/models/FundingModel.ts
@@ -0,0 +1,59 @@
+import type {MultilingualContent} from "@/models/Common";
+import type {DocumentFileResponse} from "@/models/DocumentFileModel";
+
+export interface Funding {
+    id?: number;
+    internalIdentifiers: string[];
+    oldIds: string[];
+    mergedIds: number[];
+    doi?: string;
+    grantAgreementId?: string;
+    projectId: number;
+    agreements: DocumentFileResponse[];
+    fundingParts: FundingPart[];
+    name: MultilingualContent[];
+    description: MultilingualContent[];
+    nameAbbreviation: MultilingualContent[];
+    keywords: MultilingualContent[];
+    researchAreasId: number[];
+    uris: string[];
+    fundingCallId?: number;
+    funderId?: number;
+    displayCall: MultilingualContent[];
+    displayProgram: MultilingualContent[];
+    displayFunder: MultilingualContent[];
+    fundingTypes: FundingType[];
+    amount?: MonetaryAmount;
+    competitive?: boolean;
+    renewable?: boolean;
+    dateSubmitted?: string;
+    dateAwarded?: string;
+    dateFrom?: string;
+    dateTo?: string;
+    oaMandated?: boolean;
+    oaMandateUrl?: string;
+}
+
+export interface MonetaryAmount {
+    currencyId: number;
+    amount: number;
+}
+
+export enum FundingType {
+    GRANT= "GRANT",
+    CALL= "CALL",
+    OTHER = "OTHER",
+}
+
+export interface FundingPart {
+    id?: number;
+    fundingId: number;
+    description: MultilingualContent[];
+    amount: MonetaryAmount;
+    projectEventId?: number;
+    projectDocumentId?: number;
+    fundingApplicationId?: number;
+    personProjectContributionId?: number;
+    organisationUnitProjectContributionId?: number;
+}
+
