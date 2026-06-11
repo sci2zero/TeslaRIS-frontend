b980c4672089b4861a347b01adad39b8df85613c
Ivan Mrsulja <mrsuljaivancic@gmail.com>
2026-06-11 19:48:32 +0200

    Fix build failure issue.

--- src/main.ts
+++ src/main.ts
@@ -9,19 +9,25 @@ import './assets/vuetify-custom.scss';
 import './assets/main.css';
 import i18n from './i18n';
 import { createHead } from "@vueuse/head";
-import { initializeLanguageDetector } from "./utils/LanguageDetector.js";
+import { initializeLanguageDetector } from "./utils/LanguageDetector";
 
-const pinia = createPinia();
-const head = createHead();
-await initializeLanguageDetector();
 
-loadFonts();
-i18n.setup();
+async function bootstrap() {
+    const pinia = createPinia();
+    const head = createHead();
 
-createApp(App)
-    .use(router)
-    .use(pinia)
-    .use(vuetify)
-    .use(i18n.vueI18n)
-    .use(head)
-    .mount("#app");
+    await initializeLanguageDetector();
+
+    loadFonts();
+    i18n.setup();
+
+    createApp(App)
+        .use(router)
+        .use(pinia)
+        .use(vuetify)
+        .use(i18n.vueI18n)
+        .use(head)
+        .mount("#app");
+}
+
+bootstrap();
