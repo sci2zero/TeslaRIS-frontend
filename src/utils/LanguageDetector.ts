70911436fa7516c33b4857b6d4d38a500587a592
Milos Popovic <misa01523@gmail.com>
2026-07-08 11:52:31 +0200

    Temporary disabled language detector

--- src/utils/LanguageDetector.ts
+++ src/utils/LanguageDetector.ts
@@ -1,5 +1,6 @@
 import UserService from '@/services/UserService';
-import { eld } from 'eld';
+// Temporary disabled
+// import { eld } from 'eld';
 
 
 let initialized = false;
@@ -7,7 +8,7 @@ let initialized = false;
 
 export async function initializeLanguageDetector(): Promise<void> {
     if (!initialized) {
-        await (eld as typeof eld & { load(): Promise<void> }).load();
+        // await (eld as typeof eld & { load(): Promise<void> }).load();
         initialized = true;
     }
 }
@@ -19,15 +20,17 @@ export async function detectLanguage(content: string): Promise<string | null> {
 
     await initializeLanguageDetector();
 
-    const result = eld.detect(content);
+    // const result = eld.detect(content);
 
     const loggedInUser = UserService.getLoggedInUser();
-    let locale = result?.language.toUpperCase() ?? 
-        (await loggedInUser).data?.preferredReferenceCataloguingLanguage.toUpperCase();
+    // let locale = result?.language.toUpperCase() ?? 
+    //     (await loggedInUser).data?.preferredReferenceCataloguingLanguage.toUpperCase();
 
-    if (locale === "HR") {
-        locale = "SR";
-    }
+    // if (locale === "HR") {
+    //     locale = "SR";
+    // }
+
+    let locale = "SR"
 
     return locale;
 }
